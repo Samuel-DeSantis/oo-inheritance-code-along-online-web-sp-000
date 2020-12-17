9ca48b455ee0233d2502185bbcf8750069042e21
Samuel DeSantis <samuel.i.desantis@gmail.com>
2020-12-17 02:27:39 +0000

    Automatically backed up by Learn

--- lib/car.rb
+++ lib/car.rb
@@ -2,7 +2,10 @@ require_relative "./vehicle.rb"
 
 class Car < Vehicle
 
+<<<<<<< HEAD
   def go
     'VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!'
   end
+=======
+>>>>>>> 8e96fdc43d3acbb6f136bd0cf2de54d24059c712
 end
